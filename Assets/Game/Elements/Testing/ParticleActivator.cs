3df899d501cc81759730f3d1d6616fdbd1455c19
Jon Miller <millerj.2203@protonmail.com>
2024-01-05 16:27:16 +0900

    add documentation to the particle activator for clarity of purpose

--- Assets/Game/Elements/Testing/ParticleActivator.cs
+++ Assets/Game/Elements/Testing/ParticleActivator.cs
@@ -8,7 +8,7 @@ public class ParticleActivator : MonoBehaviour
     [SerializeField] KeyCode sparksDisableKey = KeyCode.Alpha3;
     [SerializeField] KeyCode sparksReparentKey = KeyCode.Alpha4;
     [Space]
-    [SerializeField] Transform sparksParent;
+    [SerializeField] Transform otherObject;
     
     IParticleEffect retrievedSparks;
 
@@ -16,20 +16,26 @@ public class ParticleActivator : MonoBehaviour
     {
         if (Input.GetKeyDown(explosionKey))
         {
-            GameCoordinator.Instance.RequestEffect(ParticleIDs.Explosion).ISetActive();
+            // Simulates any scenario which requires an explosion at an object's position
+            IParticleEffect effect = GameCoordinator.Instance.RequestEffect(ParticleIDs.Explosion);
+            effect.ISetPosition(otherObject.localPosition);
+            effect.ISetActive();
         }
         else if (Input.GetKeyDown(sparksEnableKey))
         {
+            // Simulates a scenario where an object (maybe a building in an RTS) keeps track of the effect for later dismissal
             retrievedSparks = GameCoordinator.Instance.RequestEffect(ParticleIDs.Sparks);
             retrievedSparks.ISetActive();
         }
         else if (Input.GetKeyDown(sparksDisableKey))
         {
+            // Simulates a scenario where an object has been repaired, destroyed, etc., and no longer needs the effect
             retrievedSparks?.ISetInactive();
         }
         else if (Input.GetKeyDown(sparksReparentKey))
         {
-            retrievedSparks?.ISetParent(sparksParent);
+            // Simulates a scenario where a moving object needs the effect to follow its motion
+            retrievedSparks?.ISetParent(otherObject);
         }
     }
 }
