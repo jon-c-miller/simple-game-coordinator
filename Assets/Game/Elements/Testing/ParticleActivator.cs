bcfaf1479086a6818130c2d6606447676b60c6b3
Jon Miller <millerj.2203@protonmail.com>
2024-01-05 14:02:39 +0900

    add an activator for testing the particle manager api

---
+++ Assets/Game/Elements/Testing/ParticleActivator.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+/// <summary> Acts as a proxy for areas of the codebase that would request particle effects through GameCoordinator. </summary>
+public class ParticleActivator : MonoBehaviour
+{
+    [SerializeField] KeyCode explosionKey = KeyCode.Alpha1;
+    [SerializeField] KeyCode sparksEnableKey = KeyCode.Alpha2;
+    [SerializeField] KeyCode sparksDisableKey = KeyCode.Alpha3;
+    [SerializeField] KeyCode sparksReparentKey = KeyCode.Alpha4;
+    [Space]
+    [SerializeField] Transform sparksParent;
+    
+    IParticleEffect retrievedSparks;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(explosionKey))
+        {
+            GameCoordinator.Instance.RequestEffect(ParticleIDs.Explosion);
+        }
+        else if (Input.GetKeyDown(sparksEnableKey))
+        {
+            retrievedSparks = GameCoordinator.Instance.RequestEffect(ParticleIDs.Sparks);
+        }
+        else if (Input.GetKeyDown(sparksDisableKey))
+        {
+            retrievedSparks?.IReturnEffect();
+        }
+        else if (Input.GetKeyDown(sparksReparentKey))
+        {
+            retrievedSparks?.ISetParent(sparksParent);
+        }
+    }
+}
