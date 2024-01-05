4103e86236af74ceff425db25582d6ea775cd50d
Jon Miller <millerj.2203@protonmail.com>
2024-01-05 09:44:57 +0900

    add sound manager api to game coordinator

--- Assets/Game/Elements/GameCoordinator/GameCoordinator.cs
+++ Assets/Game/Elements/GameCoordinator/GameCoordinator.cs
@@ -1,14 +1,19 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameCoordinator : MonoBehaviour
 {
     // Initialized plain classes and editor refs to primary game systems (sound controller, etc.)
-
+    
+    [SerializeField] SoundManager soundManager;
 
 
     // Methods for common commands that will be accessed by the rest of the codebase
     
+    public void RequestSound(SoundIDs sound)
+    {
+        if (soundManager != null)
+            soundManager.PlaySound(sound);
+    }
 
     
     public static GameCoordinator Instance { get; private set; }
