764d4ac85e2dca96b909207e567b4ff220e86624
Jon Miller <millerj.2203@protonmail.com>
2024-01-05 10:00:29 +0900

    remove superfluous clips from sound manager

--- Assets/Game/Elements/SoundManager/Sounds.cs
+++ Assets/Game/Elements/SoundManager/Sounds.cs
@@ -3,9 +3,6 @@ using UnityEngine;
 [System.Serializable]
 public class Sounds
 {
-    public AudioClip PageTurnClip;
     public AudioSource PageTurn;
-    [Space]
-    public AudioClip MenuNavigationClip;
     public AudioSource MenuNavigation;
 }
